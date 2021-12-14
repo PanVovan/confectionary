02bccca60e190f3bf6d3185a52a0d35b8173e38c
Kirill Gorbach <kirill.gorbach@gmail.com>
2021-12-14 20:29:13 +0300

    Перед мержем на всякий случай

---
+++ app/src/main/java/com/confectinary/app/fragments/adapter/ConfectionersAdapter.kt
@@ -0,0 +1,56 @@
+package com.confectinary.app.fragments.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import com.confectinary.app.R
+import com.confectinary.app.db.entity.ClientDb
+import com.confectinary.app.db.entity.ConfectionerDb
+
+class ConfectionersAdapter : RecyclerView.Adapter<ConfectionersAdapter.ViewHolder>() {
+
+    var values = emptyList<ConfectionerDb>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_talbeitem, parent, false)
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        val firstname = item.firstname
+        val lastname = item.lastname
+        val patronymic = item.patronymic?:""
+        val salary = item.salary
+        val experience = item.experience
+        val rating = item.rating
+
+        val displayTest = "Имя: $firstname\n" +
+                "Фамилия: $lastname\n" +
+                "Отчество: $patronymic\n" +
+                "Зарплата: $salary\n" +
+                "Опыт: $experience\n" +
+                "Рейтинг: $rating"
+
+        holder.description.text = displayTest
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: View) : RecyclerView.ViewHolder(binding) {
+        val description: TextView = binding.findViewById(R.id.description)
+
+        override fun toString(): String {
+            return super.toString() + " '" + description.text + "'"
+        }
+    }
+
+}
---
+++ app/src/main/java/com/confectinary/app/fragments/viewfragments/ViewConfectionersFragment.kt
@@ -0,0 +1,61 @@
+package com.confectinary.app.fragments.viewfragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.confectinary.app.R
+import com.confectinary.app.databinding.FragmentViewBinding
+import com.confectinary.app.fragments.adapter.ClientsAdapter
+import com.confectinary.app.fragments.adapter.ConfectionersAdapter
+import com.confectinary.app.fragments.adapter.entity.TableNames
+
+class ViewConfectionersFragment : Fragment() {
+
+    //Меняем для разных таблиц
+    private var _binding: FragmentViewBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+    //Меняем для разных таблиц
+    private var tableName = TableNames.TablesEnum.Confectioner.value
+    //Меняем для разных таблиц
+    private var myAdapter: ConfectionersAdapter = ConfectionersAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentViewBinding.inflate(inflater, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.title = tableName
+
+        with(binding) {
+            talbeitemsList.adapter = myAdapter
+            addItem.setOnClickListener {
+                findNavController().navigate(
+                    //Меняем для разных таблиц
+                    R.id.action_viewClientsFragment_to_insertClientFragment
+                )
+            }
+        }
+
+        return binding.root
+    }
+
+    //будет срабатывать, когда мы создаем или возвращаемся во фрагмент (то есть всегда)
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onStart() {
+        super.onStart()
+        //получаем данные из бд
+//        myAdapter.values = newData
+//        myAdapter.notifyDataSetChanged()
+    }
+}
