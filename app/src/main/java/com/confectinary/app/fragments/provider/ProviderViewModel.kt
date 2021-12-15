8047a5b2afc912a5fa728605278def9c6394fbc8
Vladimir_notebook <vova.antipenkov@gmail.com>
2021-12-15 14:39:18 +0300

    изменил вьюмодели

--- app/src/main/java/com/confectinary/app/fragments/provider/ProviderViewModel.kt
+++ app/src/main/java/com/confectinary/app/fragments/provider/ProviderViewModel.kt
@@ -9,6 +9,7 @@ import com.confectinary.app.db.entity.ClientDb
 import com.confectinary.app.db.entity.ConfectionaryDb
 import com.confectinary.app.db.entity.IngredientTypeDb
 import com.confectinary.app.db.entity.ProviderDb
+import com.confectinary.app.db.entity.relation.provider_with_confectionary.ProviderAndConfectionaryCrossRef
 import com.confectinary.app.db.entity.relation.provider_with_ingredient.ProviderAndIngredientTypeCrossRef
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -21,17 +22,23 @@ class ProviderViewModel(
     private val _dataFlow = MutableSharedFlow<List<ProviderDb>>()
     val dataFlow = _dataFlow.asSharedFlow()
 
-    var ingredientTypes: List<IngredientTypeDb> = emptyList()
+
+    private val _dataFlow1 = MutableSharedFlow<List<IngredientTypeDb>>()
+    val dataFlow1 = _dataFlow1.asSharedFlow()
+
+    private val _dataFlow2 = MutableSharedFlow<List<ConfectionaryDb>>()
+    val dataFlow2 = _dataFlow2.asSharedFlow()
 
 
     fun loadProviders() {
         viewModelScope.launch {
             _dataFlow.emit(db?.getProviderDao()?.getProviders() ?: emptyList())
-            ingredientTypes = db?.getIngredientTypeDao()?.getIngredientTypes() ?: emptyList()
+            _dataFlow1.emit(db?.getIngredientTypeDao()?.getIngredientTypes() ?: emptyList())
+            _dataFlow2.emit(db?.getConfectionaryDao()?.getConfectionaries() ?: emptyList())
         }
     }
 
-    fun insert(newItem: ProviderDb, ingredientId: Long){
+    fun insert(newItem: ProviderDb, ingredientId: Long, confectionaryId: Long){
         viewModelScope.launch {
             val id = db?.getProviderDao()?.insertProvider(newItem)
 
@@ -39,6 +46,9 @@ class ProviderViewModel(
                id!!, ingredientId
             )
             db?.getProviderAndIngredientTypeDao()?.insertJunction(junction)
+            db?.getProviderAndConfectionaryDao()?.insertJunction(
+                ProviderAndConfectionaryCrossRef(id!!, confectionaryId)
+            )
         }
         loadProviders()
     }
