903a2c3bc23880be2fe1a969ee5319d8c3add457
Vladimir_notebook <vova.antipenkov@gmail.com>
2021-12-15 16:21:30 +0300

    добавил ордер

---
+++ app/src/main/java/com/confectinary/app/fragments/order/OrdersAdapter.kt
@@ -0,0 +1,42 @@
+package com.confectinary.app.fragments.order
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.confectinary.app.R
+import com.confectinary.app.db.entity.OrderDb
+
+class OrdersAdapter : RecyclerView.Adapter<OrdersAdapter.ViewHolder>() {
+
+    var values = emptyList<OrderDb>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_talbeitem, parent, false)
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        val firstname = item.description
+
+        val displayTest = "Описание: $firstname"
+
+        holder.description.text = displayTest
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: View) : RecyclerView.ViewHolder(binding) {
+        val description: TextView = binding.findViewById(R.id.description)
+
+        override fun toString(): String {
+            return super.toString() + " '" + description.text + "'"
+        }
+    }
+}
